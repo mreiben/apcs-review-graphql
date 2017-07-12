ee9904558f05686f5f6b28cec756ce5328bcc060
Jason Eiben <jason.eiben@gmail.com>
2017-07-12 17:49:39 -0400

    add logout mutation in header

--- client/index.js
+++ client/index.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ApolloClient from 'apollo-client';
+import ApolloClient, { createNetworkInterface } from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
 import { Router, hashHistory, Route, IndexRoute } from 'react-router';
 import App from './components/App';
+
+//allows cookies to be sent along with network requests
+const networkInterface = createNetworkInterface({
+  uri: '/graphql',
+  opts: {
+    credentials: 'same-origin'
+  }
+});
 
 //interacts with the backend
 const client = new ApolloClient({
   //identifies records from the server so Apollo can identify it
   //allows Apollo to uniquely identify each record
-  dataIdFromObject: o => o.id
+  dataIdFromObject: o => o.id,
+  //force cookies to be sent along with any request
+  networkInterface
 });
 
 const Root = () => {
