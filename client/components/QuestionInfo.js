384774873f9b7146da3c5b97f9ef38c63508659d
mreiben <jason.eiben@gmail.com>
2017-08-04 19:12:32 -0400

    add pagination to user profile for user questions, style questions view in user profile

--- client/components/QuestionInfo.js
+++ client/components/QuestionInfo.js
@@ -51,102 +51,125 @@ class QuestionInfo extends Component {
     }).then(this.handleCloseModal())
   }
 
-  renderInfo(){
-    if(this.props.data.loading){
-      return(
-        <div className="progress">
-          <div className="indeterminate"></div>
-        </div>
-      )
+  handleCommentDelete(){
+    console.log("deleting comment...");
+  }
+
+  renderComments(comments){
+    if(comments.length == 0){
+      return "";
     }
     else{
-      let question = this.props.data.question;
-      let rating = 0;
-      if (question.votes == 0){
-        rating = "unrated";
-      } else {
-        rating = (question.upVotes / question.votes) * 5;
-        rating = rating.toFixed(2);
+      let parsedComments = comments.map((comment) =>{
+        let userName = comment.substring(0, comment.indexOf(":"));
+        let message = comment.substring(comment.indexOf(":")+ 1);
+        return (
+          <div key={comment} className="comment" key={comment}>
+            <i
+              className="material-icons danger-icon"
+              onClick={()=>{this.handleCommentDelete(comment)}}
+              >delete</i>
+              <span className="resultCat">{userName}:</span><ReactMarkdown source={message} />
+            </div>
+          )
+        });
+        return <div className="comment-holder profile-comments-holder">{parsedComments}</div>;
       }
-      const qId = question.id;
-      const modalStyle = {
-        overlay: {
-          backgroundColor: '#f44336',
-          position: 'fixed',
-          borderRadius: '4px',
-          height: '210px',
-          left: 100,
-          right: 100,
-          top: 100
-        },
-        content: {
-          position                   : 'absolute',
-          top                        : '10px',
-          left                       : '10px',
-          right                      : '10px',
-          bottom                     : '10px',
-          border                     : '1px solid #ccc',
-          background                 : '#fff',
-          overflow                   : 'auto',
-          WebkitOverflowScrolling    : 'touch',
-          borderRadius               : '4px',
-          outline                    : 'none',
-          padding                    : '20px'
+    }
+
+    renderInfo(){
+      if(this.props.data.loading){
+        return(
+          <div className="progress">
+            <div className="indeterminate"></div>
+          </div>
+        )
+      }
+      else{
+        let question = this.props.data.question;
+        let up = question.upVoters.length;
+        let down = question.downVoters.length;
+        let rating = ((up + 5)/(up + down + 5) * 5).toFixed(1);
+        const qId = question.id;
+        const modalStyle = {
+          overlay: {
+            backgroundColor: '#f44336',
+            position: 'fixed',
+            borderRadius: '4px',
+            height: '210px',
+            left: 100,
+            right: 100,
+            top: 100
+          },
+          content: {
+            position                   : 'absolute',
+            top                        : '10px',
+            left                       : '10px',
+            right                      : '10px',
+            bottom                     : '10px',
+            border                     : '1px solid #ccc',
+            background                 : '#fff',
+            overflow                   : 'auto',
+            WebkitOverflowScrolling    : 'touch',
+            borderRadius               : '4px',
+            outline                    : 'none',
+            padding                    : '20px'
+          }
+        }
+
+        return(
+          <CollapsibleItem key={qId} header={question.prompt.substring(0,80) + "..."} icon='arrow_drop_down'>
+            <div className="collection">
+              <div className="collection-item"><code className="code">{question.code}</code></div>
+              <div className="answer"><Icon className="collection-icon green-icon">check_circle</Icon><ReactMarkdown source={question.correct}/></div>
+              <div className="answer"><Icon className="collection-icon red-icon">block</Icon><ReactMarkdown source={question.answer2}/></div>
+              <div className="answer"><Icon className="collection-icon red-icon">block</Icon><ReactMarkdown source={question.answer3}/></div>
+              <div className="answer"><Icon className="collection-icon red-icon">block</Icon><ReactMarkdown source={question.answer4}/></div>
+              <div className="answer"><Icon className="collection-icon red-icon">block</Icon><ReactMarkdown source={question.answer5}/></div>
+              <div className="collection-item"><Icon className="collection-icon blue-icon">lightbulb_outline</Icon><ReactMarkdown source={question.explanation} /></div>
+              <div className="collection-item"><Icon className="collection-icon blue-icon">list</Icon>{question.topics.map((topic)=>{return <span className="topic-list" key={topic}>{topic}</span>})}</div>
+              <div className="collection-item"><Icon className="collection-icon blue-icon">thumbs_up_down</Icon>Rating: {rating}/5.0</div>
+              <div className="collection-item"><Icon className="collection-icon blue-icon">chat</Icon>
+              Comments: {this.renderComments(question.comments)}
+            </div>
+            <div className="section">
+              <div
+                className="btn btn-special btn-left"
+                onClick={() => this.handleEditClick(qId)}
+                >Edit Question</div>
+                <div className="btn btn-danger right btn-right" onClick={this.handleOpenModal}>Delete Question</div>
+              </div>
+              <ReactModal
+                isOpen={this.state.showModal}
+                contentLabel="Delete Warning"
+                style={modalStyle}
+                >
+                  <h5>Are you sure you want to delete this question?</h5>
+                  <p>This action can't be undone!</p>
+                  <div className="btn btn-special" onClick={this.handleCloseModal}>Cancel</div>
+                  <div className="btn btn-danger" onClick={this.deleteQuestion}>I'm sure, delete!</div>
+                </ReactModal>
+              </div>
+            </CollapsibleItem>
+          )
         }
       }
 
-      return(
-        <CollapsibleItem header={question.prompt.substring(0,80) + "..."} icon='arrow_drop_down'>
-          <div className="collection">
-            <div className="collection-item"><Icon className="collection-icon icon-blue">live_help</Icon><ReactMarkdown source={question.prompt}/></div>
-            <div className="collection-item"><code className="code">{question.code}</code></div>
-            <div className="answer"><Icon className="collection-icon">check_circle</Icon><ReactMarkdown source={question.correct}/></div>
-            <div className="answer"><Icon className="collection-icon">block</Icon><ReactMarkdown source={question.answer2}/></div>
-            <div className="answer"><Icon className="collection-icon">block</Icon><ReactMarkdown source={question.answer3}/></div>
-            <div className="answer"><Icon className="collection-icon">block</Icon><ReactMarkdown source={question.answer4}/></div>
-            <div className="answer"><Icon className="collection-icon">block</Icon><ReactMarkdown source={question.answer5}/></div>
-            <div className="collection-item"><Icon className="collection-icon">lightbulb_outline</Icon><ReactMarkdown source={question.explanation} /></div>
-            <div className="collection-item"><Icon className="collection-icon">list</Icon>{question.topics.map((topic)=>{return <span className="topic-list" key={topic}>{topic}</span>})}</div>
-            <div className="collection-item"><Icon className="collection-icon">thumbs_up_down</Icon>Rating: {rating}/5.0</div>
+      render(){
+        return(
+          <div className="collection-item">
+            {this.renderInfo()}
           </div>
-          <div>
-            <div
-              className="btn btn-special btn-left"
-              onClick={() => this.handleEditClick(qId)}
-            >Edit</div>
-            <div className="btn btn-danger right btn-right" onClick={this.handleOpenModal}>Delete</div>
-          </div>
-          <ReactModal
-            isOpen={this.state.showModal}
-            contentLabel="Delete Warning"
-            style={modalStyle}
-          >
-            <h5>Are you sure you want to delete this question?</h5>
-            <p>This action can't be undone!</p>
-            <div className="btn btn-special" onClick={this.handleCloseModal}>Cancel</div>
-            <div className="btn btn-danger" onClick={this.deleteQuestion}>I'm sure, delete!</div>
-          </ReactModal>
-
-        </CollapsibleItem>
-      )
+        )
+      }
     }
-  }
 
-  render(){
-    return(
-      <div className="collection-item">
-        {this.renderInfo()}
-      </div>
-    )
-  }
-}
-
-export default graphql(currentUser)(
-  graphql(questionDelete, {
-    options: (props) => ({ variables: { id: props.id } } )
-  })(
-    graphql(query, {
-      options: (props) => ({ variables: { id: props.id } } )
-    })(QuestionInfo)
-  )
-);
+    export default graphql(currentUser)(
+      graphql(questionDelete, {
+        options: (props) => ({ variables: { id: props.id } } )
+      })(
+        graphql(query, {
+          options: (props) => ({ variables: { id: props.id } } )
+        })(QuestionInfo)
+      )
+    );
--- client/components/UserProfile.js
+++ client/components/UserProfile.js
@@ -3,26 +3,49 @@ import { graphql } from 'react-apollo';
 import query from '../queries/CurrentUser';
 import { Link } from 'react-router';
 import UserQuestions from './UserQuestions';
-import { Preloader } from 'react-materialize';
+import { Preloader, Pagination } from 'react-materialize';
 
 class UserProfile extends Component {
   constructor(props){
     super(props);
+    this.state = {
+      questionPage: 1
+    }
+  }
+
+  handleQuestionPageSelect(p){
+    this.setState({ questionPage: p});
   }
 
   render(){
+
     if(this.props.data.loading){ return <Preloader size='small' flashing /> }
-    return(
-      <div>
-        <div className="section">
-          <Link to="/dashboard" className="btn btn-special">Back</Link>
+    else{
+
+      let questions = this.props.data.user.questions;
+      let qCount = questions.length;
+      let pages = Math.ceil(qCount / 5);
+      let p = this.state.questionPage - 1;
+      let subQuestions = questions.slice(p, p + 5);
+
+      return(
+        <div>
+          <div className="section">
+            <Link to="/dashboard" className="btn btn-special">Back</Link>
+          </div>
+          <h3>User Profile: {this.props.data.user.name}</h3>
+          <p>Email: {this.props.data.user.email}</p>
+          <h5>My Questions:</h5>
+          <Pagination
+            items={pages}
+            activePage={1}
+            maxButtons={5}
+            onSelect={(p) => {this.handleQuestionPageSelect(p)}}
+          />
+          <UserQuestions questions={subQuestions} />
         </div>
-        <h3>User Profile: {this.props.data.user.name}</h3>
-        <p>Email: {this.props.data.user.email}</p>
-        <h5>My Questions:</h5>
-        <UserQuestions questions={this.props.data.user.questions} />
-      </div>
-    );
+      );
+    }
   }
 }
 
