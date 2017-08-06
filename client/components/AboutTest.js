9b4c66d21f83c96547dd36f9dc0e3bff8ad80e1f
mreiben <jason.eiben@gmail.com>
2017-08-06 16:14:47 -0400

    direct links to new tab on test info view

--- client/components/AboutTest.js
+++ client/components/AboutTest.js
@@ -25,26 +25,26 @@ const AboutTest = () => {
           <div className="collection">
             <h5 className="collection-item">Exam Topics</h5>
             <div className="collection-item topics-list-holder">
-              <p>While the course covers a wide array of topics related to software engineering, the written exam primarily focuses on the following topics:</p>
+              <p>While the course covers a wide array of topics related to software engineering, the written exam primarily focuses on the topics below. Click on the links to learn more about each topic:</p>
               <ul className="topics-list">
-                <li>The AP Java Subset <a href="https://apcentral.collegeboard.org/courses/ap-computer-science-a/classroom-resources/ap-java-subset-language-features?course=ap-computer-science-a"><i className="material-icons redirect-icon">open_in_new</i></a></li>
-                <li>Numerical representations of integers <a href="http://www.learning-about-computers.com/tutorials/binary_and_hexidecimal.shtml"><i className="material-icons redirect-icon">open_in_new</i></a></li>
-                <li>Primitive data types (int, boolean, double) <a href="https://docs.oracle.com/javase/tutorial/java/nutsandbolts/datatypes.html"><i className="material-icons redirect-icon">open_in_new</i></a></li>
-                <li>Assignment Operators <a href="https://docs.oracle.com/javase/tutorial/java/nutsandbolts/op1.html"><i className="material-icons redirect-icon">open_in_new</i></a></li>
-                <li>Equality / Relational / Conditional Operators <a href="https://docs.oracle.com/javase/tutorial/java/nutsandbolts/op2.html"><i className="material-icons redirect-icon">open_in_new</i></a></li>
-                <li>Strings <a href="https://www.tutorialspoint.com/java/java_strings.htm"><i className="material-icons redirect-icon">open_in_new</i></a></li>
-                <li>Classes <a href="https://docs.oracle.com/javase/tutorial/java/javaOO/classes.html"><i className="material-icons redirect-icon">open_in_new</i></a></li>
-                <li>Lists <a href="https://docs.oracle.com/javase/tutorial/collections/interfaces/list.html"><i className="material-icons redirect-icon">open_in_new</i></a></li>
-                <li>Arrays (1-dimensional and 2-dimensional) <a href="https://docs.oracle.com/javase/tutorial/java/nutsandbolts/arrays.html"><i className="material-icons redirect-icon">open_in_new</i></a></li>
-                <li>Conditional Logic <a href="https://docs.oracle.com/javase/tutorial/java/nutsandbolts/if.html"><i className="material-icons redirect-icon">open_in_new</i></a></li>
-                <li>Loops <a href="https://docs.oracle.com/javase/tutorial/java/nutsandbolts/for.html"><i className="material-icons redirect-icon">open_in_new</i></a></li>
-                <li>Searching <a href="http://www.java2novice.com/java-search-algorithms/"><i className="material-icons redirect-icon">open_in_new</i></a></li>
-                <li>Sorting <a href="http://www.java2novice.com/java-sorting-algorithms/"><i className="material-icons redirect-icon">open_in_new</i></a></li>
-                <li>Runtime exceptions<a href="https://stackoverflow.com/questions/1502860/what-are-the-most-commonly-used-runtime-exceptions-in-java"><i className="material-icons redirect-icon">open_in_new</i></a></li>
-                <li>Program correctness<a href="http://www.cs.colostate.edu/~cs122/.Fall14/tutorials/tut_5.php"><i className="material-icons redirect-icon">open_in_new</i></a></li>
-                <li>Algorithm Analysis<a href="https://rob-bell.net/2009/06/a-beginners-guide-to-big-o-notation/"><i className="material-icons redirect-icon">open_in_new</i></a></li>
+                <li>The AP Java Subset <a target="_subset" href="https://apcentral.collegeboard.org/courses/ap-computer-science-a/classroom-resources/ap-java-subset-language-features?course=ap-computer-science-a"><i className="material-icons redirect-icon">open_in_new</i></a></li>
+                <li>Numerical representations of integers <a target="_numbers" href="http://www.learning-about-computers.com/tutorials/binary_and_hexidecimal.shtml"><i className="material-icons redirect-icon">open_in_new</i></a></li>
+                <li>Primitive data types (int, boolean, double) <a target="_primitives" href="https://docs.oracle.com/javase/tutorial/java/nutsandbolts/datatypes.html"><i className="material-icons redirect-icon">open_in_new</i></a></li>
+                <li>Assignment Operators <a target="_assignment" href="https://docs.oracle.com/javase/tutorial/java/nutsandbolts/op1.html"><i className="material-icons redirect-icon">open_in_new</i></a></li>
+                <li>Equality / Relational / Conditional Operators <a target="_operators" href="https://docs.oracle.com/javase/tutorial/java/nutsandbolts/op2.html"><i className="material-icons redirect-icon">open_in_new</i></a></li>
+                <li>Strings <a target="_strings" href="https://www.tutorialspoint.com/java/java_strings.htm"><i className="material-icons redirect-icon">open_in_new</i></a></li>
+                <li>Classes <a target="_classes" href="https://docs.oracle.com/javase/tutorial/java/javaOO/classes.html"><i className="material-icons redirect-icon">open_in_new</i></a></li>
+                <li>Lists <a target="_lists" href="https://docs.oracle.com/javase/tutorial/collections/interfaces/list.html"><i className="material-icons redirect-icon">open_in_new</i></a></li>
+                <li>Arrays (1-dimensional and 2-dimensional) <a target="_arrays" href="https://docs.oracle.com/javase/tutorial/java/nutsandbolts/arrays.html"><i className="material-icons redirect-icon">open_in_new</i></a></li>
+                <li>Conditional Logic <a target="_logic" href="https://docs.oracle.com/javase/tutorial/java/nutsandbolts/if.html"><i className="material-icons redirect-icon">open_in_new</i></a></li>
+                <li>Loops <a  target="_loops" href="https://docs.oracle.com/javase/tutorial/java/nutsandbolts/for.html"><i className="material-icons redirect-icon">open_in_new</i></a></li>
+                <li>Searching <a  target="_search" href="http://www.java2novice.com/java-search-algorithms/"><i className="material-icons redirect-icon">open_in_new</i></a></li>
+                <li>Sorting <a  target="_sort" href="http://www.java2novice.com/java-sorting-algorithms/"><i className="material-icons redirect-icon">open_in_new</i></a></li>
+                <li>Runtime exceptions <a target="_exceptions" href="https://stackoverflow.com/questions/1502860/what-are-the-most-commonly-used-runtime-exceptions-in-java"><i className="material-icons redirect-icon">open_in_new</i></a></li>
+                <li>Program correctness <a target="_correctness" href="http://www.cs.colostate.edu/~cs122/.Fall14/tutorials/tut_5.php"><i className="material-icons redirect-icon">open_in_new</i></a></li>
+                <li>Algorithm Analysis <a target="_algorithms" href="https://rob-bell.net/2009/06/a-beginners-guide-to-big-o-notation/"><i className="material-icons redirect-icon">open_in_new</i></a></li>
               </ul>
-              <p>Consider searching <a href="https://stackoverflow.com/questions/tagged/java">StackOverflow</a> for further examples and explanations.</p>
+              <p>Consider searching <a target="_stack" href="https://stackoverflow.com/questions/tagged/java">StackOverflow</a> for further examples and explanations.</p>
             </div>
           </div>
           <div className="collection">
@@ -52,10 +52,10 @@ const AboutTest = () => {
             <div className="collection-item topics-list-holder">
               <p>The resources below are a great place to start practicing for the Free Response portion of the exam:</p>
               <ul className="fr-resources">
-                <li><a href="https://apstudent.collegeboard.org/apcourse/ap-computer-science-a/exam-practice">Questions and scoring guidelines</a> from actual exams</li>
-                <li><a href="https://www.tutorialspoint.com/compile_java_online.php">CodingGround</a> - an online Java runtime environment</li>
-                <li><a href="http://codingbat.com/java">CodingBat</a> - interactive coding challenges</li>
-                <li><a href="https://codewars.com">codewars</a> - competetive programming challenges</li>
+                <li><a target="_free-response" href="https://apstudent.collegeboard.org/apcourse/ap-computer-science-a/exam-practice">Questions and scoring guidelines</a> from actual exams</li>
+                <li><a target="_codingground" href="https://www.tutorialspoint.com/compile_java_online.php">CodingGround</a> - an online Java runtime environment</li>
+                <li><a target="_codingbat" href="http://codingbat.com/java">CodingBat</a> - interactive coding challenges</li>
+                <li><a target="_codewars" href="https://codewars.com">codewars</a> - competetive programming challenges</li>
               </ul>
             </div>
           </div>
--- client/components/Dashboard.js
+++ client/components/Dashboard.js
@@ -38,28 +38,34 @@ class Dashboard extends Component {
             <Link to="/profile" className="btn dashboard-btn  btn-special">Profile</Link>
             <Link to="/about-test" className="btn dashboard-btn  btn-special">Test Info</Link>
           </div>
-          <h4>The AP® Computer Science A Exam:</h4>
-          <h5>Tuesday, May 15th 2018</h5>
-          <h5>Countdown:
-            <Countdown
-              date={'Tues, May 15 2018 12:00'}
-              renderer={renderer}
-            />
-          </h5>
           <div className="section">
+            <h4>The AP® Computer Science A Exam: 5/15/2018</h4>
+            <h5>Countdown:
+              <Countdown
+                date={'Tues, May 15 2018 12:00'}
+                renderer={renderer}
+              />
+            </h5>
+          </div>
+          <div>
+            <p>Welcome back! This page is designed to allow students and teachers to create, share,
+              and practice on multiple choice questions for the APCS exam.  While it is easy to find
+              more than a decade's worth of Free Response questions, very few multiple choice questions
+              are released by the College Board. This free resource is only useful if users actually create
+              evaluate, and practice with test-level questions - so please write one today!</p>
             <div className="collection">
               <h5 className="collection-item">App facts</h5>
               <p className="collection-item">some stats here</p>
-              </div>
-            </div>
-            <div className="footer">
-              <p>Copyright © 2017 Jason Eiben</p>
-              {/* <p className="trademark">AP® is a trademark owned by the College Board, which is not affiliated with, and does not endorse, this site.</p> */}
             </div>
           </div>
-        );
-      }
+          <div className="footer">
+            <p>Copyright © 2017 Jason Eiben</p>
+            {/* <p className="trademark">AP® is a trademark owned by the College Board, which is not affiliated with, and does not endorse, this site.</p> */}
+          </div>
+        </div>
+      );
     }
   }
+}
 
-  export default graphql(query)(Dashboard);
+export default graphql(query)(Dashboard);
